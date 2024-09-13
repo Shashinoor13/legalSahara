923d84397a0657bc222a34c749aa155bfd616a35
Shashinoor13 <shashinoorghimire13@gmail.com>
2024-09-13 23:13:46 +0545

    Fix:Async operations in mail

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,8 +1,9 @@
 import { getNews, getProjects, getSubscribedEmails } from "@/sanity/sanity-utils";
 import { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 import nodemailer from "nodemailer";
 
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
+export async function GET(req: NextRequest, res: NextResponse) {
     const _news = await getNews();
     const _blogs = await getProjects();
     const _subscribed = await getSubscribedEmails();
